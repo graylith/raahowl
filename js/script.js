aef5aa5add67f33e7df00ada4317adb93839db59
graylith <44845049+graylith@users.noreply.github.com>
2022-03-22 11:33:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+preLoader()
+
+function preLoader(){
+    var tl = gsap.timeline();
+    tl.to(".preLoader-front", {
+        duration: 1.2,
+        height:"100%",
+        top: "-100%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".preLoader-front img", {
+        duration: 1.2,
+        width:"20%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".preLoader-front", {
+        duration: 1,
+        top: "-200%",
+        ease: "Expo.easeInOut",
+        delay: 0.3,
+    });
+    
+    tl.to(".preLoader-back", {
+        duration: 1,
+        top: "-200%",
+        ease: "Expo.easeInOut",
+        delay: -1
+    });
+    
+    tl.to(".main", {
+        duration: 1,
+        width:"100%",
+        ease:"Expo.easeInout",
+        delay: -0.4
+    })
+
+    tl.from(".button-container", {
+        duration:0.6,
+        bottom:"-100%",
+        ease:"elastic",
+        
+        
+    })
+    
+}
